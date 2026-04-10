4a06972ab51e210c654438c8130f43b5a5693769
gastondeganis <gastondeganis@gmail.com>
2026-04-10 00:30:55 -0300

    nuevo websocket

--- apps/api/cmd/server/main.go
+++ apps/api/cmd/server/main.go
@@ -3,12 +3,41 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("upgrade error:", err)
+		return
+	}
+	defer conn.Close()
+
+	log.Println("new websocket client connected")
+
+	err = conn.WriteJSON(map[string]string{
+		"event":   "connected",
+		"message": "bienvenido a El Impostor",
+	})
+	if err != nil {
+		log.Println("write error:", err)
+	}
+}
 
 func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ok"))
 	})
+
+	http.HandleFunc("/ws", wsHandler)
 
 	log.Println("server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- apps/api/go.mod
+++ apps/api/go.mod
@@ -1,3 +1,5 @@
 module github.com/gastondeganis/juegos-para-trolos/api
 
 go 1.24.3
+
+require github.com/gorilla/websocket v1.5.3
